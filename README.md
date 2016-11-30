5d4dc67ce23c96984ff2df5f1246586a47f548d2
kanghonggu <kanghonggu@zuminternet.com>
2016-11-30 16:42:06 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 자바 스프링 패스트 캠퍼스 강의를 위한 프로젝트입니다.
+
+## 프로젝트 환경
+
+- Back End
+    - Java 8
+    - SpringBoot 1.4.2
+    - Spring Data JPA
+    - H2 DB Engine
+    - Maven
+
+- Front End
+    - mustache template engine
---
+++ src/main/java/com/zumgu/JavaFastCampusApplication.java
@@ -0,0 +1,12 @@
+package com.zumgu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFastCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFastCampusApplication.class, args);
+	}
+}
